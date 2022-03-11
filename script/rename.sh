e9a023d4a882393f2e7b6321f9ddd25ba0d5d280
Muhammad Ilham <muhd.iiilham@gmail.com>
2022-03-11 18:25:00 +0700

    doc: Comment To Clarify Script

--- script/rename.sh
+++ script/rename.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# This command below only tested on mac.
+# Need to test this command on LINUX env too.
 cd $1
 find . -type f -iname "*.mod*" -exec sed -i '' -e "s%$1%$2%g" {} \;
 find . -type f -iname "*.go*" -exec sed -i '' -e "s%$1%$2%g" {} \;
